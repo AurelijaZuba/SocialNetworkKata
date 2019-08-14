b8489dbef28ef895cc65acd680ab9de075ec1f5a
AurelijaZuba <auryte00@hotmail.com>
2019-08-14 12:02:10 +0100

    Refactor unit testing - extract repository and console mocks into setup Extract Post and Wall information into constant field

--- src/test/java/com/codurance/SocialServiceShould.java
+++ src/test/java/com/codurance/SocialServiceShould.java
@@ -1,50 +1,57 @@
 package com.codurance;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class SocialServiceShould {
 
+    public static final String ALICE_POST_MESSAGE = "Alice -> I love the weather today";
+    public static final String BOB_POST_MESSAGE = "Bob -> Damn! We lost!";
+    public static final String ALICE_WALL = "Alice wall";
+    public static final String BOB_WALL = "Bob wall";
+
+    private Repository repositoryMock;
+    private SocialConsole consoleMock;
+
+    @BeforeEach
+    void setUp() {
+        repositoryMock = mock(Repository.class);
+        consoleMock = mock(SocialConsole.class);
+    }
+
     @Test
     void post_single_message_for_one_user() {
-        Repository repositoryMock = mock(Repository.class);
-        SocialConsole consoleMock = mock(SocialConsole.class);
-
         SocialNetwork socialNetwork = new SocialNetwork(repositoryMock, consoleMock);
-        socialNetwork.messageParser("Alice -> I love the weather today");
+        socialNetwork.messageParser(ALICE_POST_MESSAGE);
 
         verify(repositoryMock).addMessage("Alice", "I love the weather today");
     }
 
     @Test
     void return_one_message_from_users_wall() {
-        SocialConsole consoleMock = mock(SocialConsole.class);
         Repository repository = new Repository();
         SocialNetwork socialNetwork = new SocialNetwork(repository, consoleMock);
 
-        socialNetwork.messageParser("Alice -> I love the weather today");
-        socialNetwork.messageParser("Alice wall");
+        socialNetwork.messageParser(ALICE_POST_MESSAGE);
+        socialNetwork.messageParser(ALICE_WALL);
 
         verify(consoleMock).print("Alice - I love the weather today");
     }
 
     @Test
     void return_message_for_the_user_requesting_to_that_wall() {
-        SocialConsole consoleMock = mock(SocialConsole.class);
         Repository repository = new Repository();
         SocialNetwork socialNetwork = new SocialNetwork(repository, consoleMock);
 
-        socialNetwork.messageParser("Bob -> Damn! We lost!");
-        socialNetwork.messageParser("Bob wall");
+        socialNetwork.messageParser(BOB_POST_MESSAGE);
+        socialNetwork.messageParser(BOB_WALL);
         verify(consoleMock).print("Bob - Damn! We lost!");
 
-        socialNetwork.messageParser("Alice -> I love the weather today");
-        socialNetwork.messageParser("Alice wall");
+        socialNetwork.messageParser(ALICE_POST_MESSAGE);
+        socialNetwork.messageParser(ALICE_WALL);
         verify(consoleMock).print("Alice - I love the weather today");
     }
-
-
 }
